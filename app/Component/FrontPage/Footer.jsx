4236ed7999622e14f7c27d74c7c0cb5a51d8cfbc
jonahjohnzon <jonahjohnzon@gmail.com>
2025-03-01 16:07:02 +0100

    push

--- app/Component/FrontPage/Footer.jsx
+++ app/Component/FrontPage/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useRouter } from 'nextjs-toploader/app';
 
 const Footer = () => {
+    const router = useRouter();
+
   return (
     <div className=' w-full flex flex-col items-center justify-center  bg-blue-950' >
         <div className=' py-10 flex flex-col md:flex-row w-[90%] xl:w-2/3 justify-between'>
@@ -11,15 +14,14 @@ const Footer = () => {
                 <h1 className='  sm:text-xl font-semibold mb-3 '>Get In Touch</h1>
                 <div className=' text-sm sm:text-base flex flex-col w-1/2 mb-10 md:mb-0  '>
                     <p>NO. 11, ROAD 7, SUNVIEW ESTATE, OPPOSITE CROWN ESTATE, SANGOTEDO-AJAH, , SANGOTEDO-AJAH, LAGOS STATE</p>
-                    <p>gmercycrownlimitedcso@gmail.com</p>
+                    <p>Email: gmercycrownlimitedcso@gmail.com</p>
                 </div>
             </div>
             <div>
                 <h1 className='  sm:text-xl font-semibold mb-3 whitespace-nowrap  '>Quick Link</h1>
                 <div className='text-sm sm:text-base '>
-                <p className=' flex flex-col w-1/2 '>Home</p>
-                <p className=' flex flex-col w-1/2 '>Application</p>
-                <p className=' flex flex-col w-1/2 '>About Us</p>
+                <p className=' flex flex-col w-1/2 cursor-pointer hover:scale-105' onClick={()=>router.push('/')}>Home</p>
+                <p className=' flex flex-col w-1/2 cursor-pointer hover:scale-105' onClick={()=>router.push('/contact')}>Contact Us</p>
                 </div>
             </div>
         </div>
